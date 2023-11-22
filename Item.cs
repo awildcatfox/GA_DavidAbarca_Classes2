af3e3aa98f2a7927268a226a1965248895d6dcaf
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-11-21 19:01:27 -0800

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_DavidAbarca_Classes2
+{ 
+
+    public class Item
+    {
+        // Fields
+        string _name;
+        string _description;
+        double _price;
+        double _discount;
+
+        // Constructor
+        public Item(string name, string description, double price, double discount)
+        {
+            // Field = argument
+            _name = name;
+            _description = description;
+            _price = price;
+            _discount = discount;
+
+        } // Item
+          // Take a name, and a price
+        public Item(string name, double price)
+        {
+            _name = name;
+            _price = price;
+            _description = "";
+            _discount = 0;
+        }
+
+
+        // access modifier - Return Type - Name ( Same as the Field )
+        // DOES NOT TAKE PARAMETERS
+        public string Name
+        {
+            // This is a get, it lets the user READ the value
+            get
+            {
+                return _name;
+
+
+            }
+
+
+            set
+            {
+                _name = value;
+            }
+
+
+
+        }
+
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+
+
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                // Prevent a negative number from being ASSIGNED to an item
+                if (value >= 0)
+                {
+                    _price = value;
+                }
+            }
+        } // Price
+
+
+        public double Discount
+        {
+            get { return _discount; }
+            set
+            {
+                // 0 --------------------- 1
+                if (value >= 0 && value <= 1)
+                {
+                    _discount = value;
+                }
+            }
+        }
+
+
+        // Class Method
+        public double DiscountedAmount()
+        {
+            return Price * Discount;
+        }
+        public double CalculateTotalPrice()
+        {
+            return Price - DiscountedAmount();
+        }
+
+        // OVERRIDE ToString()
+        public override string ToString()
+        {
+            // Name: Granny Smith - Price: $1.50 - Discount: $0.15 - Total Price: $1.35
+            return $"Name: {_name} - Price: {_price.ToString("c")} - Discount:  {DiscountedAmount().ToString("c")} - Total Price: {CalculateTotalPrice().ToString("c")}";
+        } // ToString()
+
+
+    }
+
+
+
+
+}
---
+++ ShopingCart.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_DavidAbarca_Classes2
+{
+
+
+    internal class ShopingCart
+    {
+        // Fields
+        string _storeName;
+        List<Item> _itemsInCart;
+        double _tax;
+
+
+
+
+        // Constructor
+        public ShopingCart(string storeName)
+        {
+            _storeName = storeName;
+            _itemsInCart = new List<Item>();
+            _tax = .1;
+        } // ShoppingCart()
+
+
+
+        // Properties
+        public string StoreName
+        {
+            get => _storeName; // This is shorthand for get
+            set => _storeName = value; // This is shorthand for set
+        } // StoreName
+
+
+        public List<Item> ItemsInCart
+        {
+            get
+            {
+                return _itemsInCart;
+            }
+        }
+
+        public double Tax
+        {
+            get => _tax;
+        } // Tax
+
+
+
+        public void AddItem(Item item)
+        {
+            _itemsInCart.Add(item);
+        } // 
+
+        public double TotalBeforeTax()
+        {
+            double sum = 0;
+            foreach (Item item in _itemsInCart)
+            {
+                sum += item.CalculateTotalPrice();
+            }
+
+            return sum;
+        } // TotalBeforeTax()
+
+
+        public double TaxOnTotal()
+        {
+            return TotalBeforeTax() * _tax;
+        } // TaxOnTotal()
+
+
+        public double TotalPrice()
+        {
+            return TotalBeforeTax() + TaxOnTotal();
+        } // TotalPrice()
+
+        public string Receipt()
+        {
+            // This C# object will help us display the exact time the receipt is processed
+            DateTime dto = DateTime.Now;
+            // Creating an empty string to start formatting.
+            string fullReceipt = "";
+
+            // Concatenating our store name into our receipt
+            fullReceipt += $"Welcome to {_storeName}\n";
+            // Displaying the current day and time on the receipt
+            fullReceipt += $"Date: {dto.ToShortDateString()} {dto.ToLongTimeString()}";
+            fullReceipt += $"\n-----\n\n";
+            fullReceipt += $"Items\n";
+
+            // Using a foreach loop, we go through the items in our cart and concatenate the .ToString() to our receipt string. This will show every item in our cart.
+            foreach (Item item in _itemsInCart)
+            {
+                fullReceipt += $"{item.ToString()}\n";
+            }
+
+            // Displays all the final calculations and the final total
+            fullReceipt += $"\n-----\n\n";
+            fullReceipt += $"Number Of Items : {_itemsInCart.Count}\n";
+            fullReceipt += $"Total Before Tax : {TotalBeforeTax().ToString("c")}\n";
+            fullReceipt += $"Tax : {TaxOnTotal().ToString("c")}\n";
+            fullReceipt += $"Total Price : {TotalPrice().ToString("c")}\n";
+            return fullReceipt;
+        } // Receipt
+
+
+    }
+
+}
